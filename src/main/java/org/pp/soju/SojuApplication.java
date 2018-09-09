41e8de1231285462fcea4da1bf0f83f3ebd3aaf5
gwiyeong <gwiyeong@nhnent.com>
2018-09-09 15:48:10 +0900

    init

---
+++ src/main/java/org/pp/soju/SojuApplication.java
@@ -0,0 +1,12 @@
+package org.pp.soju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SojuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SojuApplication.class, args);
+	}
+}
---
+++ src/test/java/org/pp/soju/SojuApplicationTests.java
@@ -0,0 +1,16 @@
+package org.pp.soju;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SojuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
